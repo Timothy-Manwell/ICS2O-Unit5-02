102a707853098a469a4de8e8b41af136ec2d3db5
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-11-16 14:36:08 -0500

    init

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022 Timothy Manwell All rights reserved
 //
 // Created by: Timothy Manwell
-// Created on: Oct 2022
+// Created on: Nov 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,14 +10,31 @@
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-02/sw.js", {
+    scope: "/ICS2O-Unit5-02/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This funtion generates and displays random number
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+ function myButtonClicked() {
+  buttonOnChecked = document.getElementById('flash1').checked
+
+  if (buttonOnChecked == true) {
+
+    //generates random positive number
+    const randomPosNum = Math.floor(Math.random() * 6) + 1
+
+    //displays random positive number
+    document.getElementById("radio-button-value").innerHTML =
+    "Your random number is: " + randomPosNum
+  } else {
+    //generates random negative number
+    const randomNegNum = Math.floor(Math.random() * (-6)) + 1
+
+    //displays random negative number
+    document.getElementById("radio-button-value").innerHTML =
+    "Your random number is: " + randomNegNum
+  }
 }
